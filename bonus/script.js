c3ce758032c9ee09a11017bd9fde68784fe111e8
Diletta Pelloni <dilpel@live.it>
2023-01-18 15:24:45 +0100

    fatta gestione profilepic

---
+++ bonus/script.js
@@ -0,0 +1,136 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": null
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+//prendo il contenitore dei post
+const container = document.getElementById('container');
+
+//creo i post
+posts.forEach(element => {
+    const post = document.createElement('div');
+    post.classList.add('post');
+
+    //GESTIONE PROFILE PIC
+    //di default prendo ciò che trovo nell'oggetto
+    let authorImage = `<img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">`
+    //se non c'è niente devo mettere uno span con le iniziali
+    if (element.author.image == null) {
+        const initials = element['author']['name'].split(" ").map(word => word.charAt(0).toUpperCase());
+        authorImage = `
+            <div class="profile-pic-default">
+                <span class="">${initials.join("")}</span>
+            </div>
+        `
+    }
+
+    post.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    ${authorImage}                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button js-like-button" href="#" data-postid="${element.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    `;
+
+    container.append(post);
+});
+
+//faccio un array con i bottoni dei like
+const likeButtons = document.querySelectorAll('.js-like-button');
+//faccio un array in cui salvare i post a cui è stato messo like
+const likeArray = [];
+
+//a ciascun bottone like assegno un evento al click
+likeButtons.forEach((element, i)=> {
+    element.addEventListener('click',
+    function() {
+        //solo se non ho già messo mi piace
+        if(!this.classList.contains('like-button--liked')) {
+            //incremento il like counter
+            const likeCounter = document.getElementById(`like-counter-${i+1}`);
+            let likeNum = parseInt(likeCounter.innerHTML);
+            likeCounter.innerHTML = likeNum + 1;
+            //mi salvo l'ID nell'apposito array
+            likeArray.push(i + 1);
+        }
+        //cambio il colore del testo
+        this.classList.add('like-button--liked');
+    })
+});
+
